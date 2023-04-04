d9c915bfe4f67e3a2207d07354029c46c21be839
Ivan Conde <icondev.silva@gmail.com>
2023-04-04 00:56:25 -0500

    cambio en servicio raiz

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('object');
+  res.send('Mi primer server en express');
 });
 
 app.get('/productos', (req, res) => {
