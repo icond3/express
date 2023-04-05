0d254437d30717ed88bee40ae494a1d30ad58be5
Ivan Conde <icondev.silva@gmail.com>
2023-04-05 17:35:27 -0600

    Se agrega get por su id

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ const app = express();
 
 const port = 3000;
 
+
 app.get('/', (req, res) => {
   res.send('Mi primer server en express');
 });
 
 app.get('/productos', (req, res) => {
+  res.json([
+    {
+      name: 'Producto1',
+      price: 1333
+    },{
+      name: 'Producto2',
+      price: 7654
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
   res.json({
+    id,
     name: 'Producto1',
     price: 1333
   });
-});
+})
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
