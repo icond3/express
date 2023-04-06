e8e3310f1b9160a5b86c504e2febe5c79b1e679e
Ivan Conde <icondev.silva@gmail.com>
2023-04-06 00:46:40 -0600

    Se agrega uso de faker para poblar informacion

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 
 const port = 3000;
@@ -8,17 +10,21 @@ app.get('/', (req, res) => {
   res.send('Mi primer server en express');
 });
 
-app.get('/productos', (req, res) => {
-  res.json([
-    {
-      name: 'Producto1',
-      price: 1333
-    },{
-      name: 'Producto2',
-      price: 7654
-    }
-  ]);
+app.get('/products', (req, res) => {
+  const products = []
+  const { size } = req.query;
+  const limit = size || 5;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt( faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products);
 });
+
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params
