8f8e0414095d9bd2a1a7609b2ae50773bc4cebea
Ivan Conde <icondev.silva@gmail.com>
2023-04-09 18:18:34 -0600

    Separacion de responsabilidades

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes')
 
 const app = express();
 
@@ -10,31 +10,8 @@ app.get('/', (req, res) => {
   res.send('Mi primer server en express');
 });
 
-app.get('/products', (req, res) => {
-  const products = []
-  const { size } = req.query;
-  const limit = size || 5;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt( faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    })
-  }
-  res.json(products);
-});
-
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params
-  res.json({
-    id,
-    name: 'Producto1',
-    price: 1333
-  });
-})
-
 app.listen(port, () => {
   console.log('Mi port ' + port);
 })
+
+routerApi(app)
