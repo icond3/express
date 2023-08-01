5a7cf2420f71ea326b1026ac0c0cbd92779be7f9
Ivan Conde Silva <icondev.silva@gmail.com>
2023-08-01 00:26:48 -0600

    cambios de mi server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes')
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Mi primer server en express');
