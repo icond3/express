f6b657beaa8b4fd93d11d0da2375dd5314b55d06
Ivan Conde Silva <icondev.silva@gmail.com>
2026-06-28 22:58:20 -0600

    Se eliminan comentarios innecesarios

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 //Desarrollo: iconde 2026
 //Date: 27-06-2026
-//Desde mi cuenta personal
-// ahroa desde mi cuenta de trabajo
+
 const express = require('express');
+
 const productsRouter = require('./products.router')
 
 function routerApi(app) {
