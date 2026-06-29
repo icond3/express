a3f56fea5f5ab3d66c1178767ef5495ba0aaf38d
Ivan Conde Silva <icondev.silva@gmail.com>
2026-06-28 22:49:10 -0600

    se agrega nuevo comentario de trabajo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 //Desarrollo: iconde 2026
 //Date: 27-06-2026
 //Desde mi cuenta personal
+// ahroa desde mi cuenta de trabajo
 const express = require('express');
 
 const productsRouter = require('./products.router')
